c1656a9d83aea69d55b9915c180803ec951acb99
Nemerd <xtremestripedtiger@gmail.com>
2022-05-17 22:35:57 -0300

    Otros cambios

--- scripts/main.js
+++ scripts/main.js
@@ -33,24 +33,32 @@ const camara = {
     camOff: document.getElementById('camOff'),
     cam: document.getElementById('cam'),
 }
-
-let microfono = {
+const microfono = {
     status: false,
     micOn: document.getElementById('micOn'),
     micOff: document.getElementById('micOff'),
     mic: document.getElementById('mic')
 }
 
+class Persona {
+    constructor(nombre, id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+}
+
 const addPart = document.getElementById("addPart");
+const delPart = document.getElementById("delPart");
 
 
 let volumen = 50;
-const opcionesPosibles = ["0", "00", "1", "2", "3", "+", "-", "salir"];
+// const opcionesPosibles = ["0", "00", "1", "2", "3", "+", "-", "salir"];
 
 const hora = document.getElementById("hora");
 
 // Event listeners
 addPart.addEventListener("click", agregar_participante);
+delPart.addEventListener("click", eliminar_participante);
 camara.cam.addEventListener("click", toggle_camara);
 microfono.mic.addEventListener("click", toggle_microfono);
 
@@ -58,17 +66,39 @@ function actualizarHora() {
     hora.textContent = `${cal.getHours()}:${cal.getMinutes()}`;
 }
 
+function randomID() {
+    let random = Math.floor(Math.random() * 101);
+
+    if (participantes.length === 0) {
+        return random;
+    }
+
+    for (let i of participantes) {
+        if (random === i.id) {
+            console.log(i.id);
+            // return randomID();
+        } else {
+            return random;
+        }
+    }
+}
+
 // Participantes
 function agregar_participante() {
     let nombre = prompt("Hola, ¿Cuál es tu nombre?");
-    participantes.push(nombre);
-    console.log(`Bienvenido/a a la reunión ${nombre}.`);
+    let novato = new Persona(nombre, randomID());
+    participantes.push(novato);
+    console.log(`Bienvenido/a a la reunión ${novato.nombre}. con ID: ${novato.id}`);
     sessionStorage.setItem('participantes', JSON.stringify(participantes));
 }
 
 function listar_participantes() {
+
     participantes = JSON.parse(sessionStorage.getItem('participantes'));
-    participantes.sort();
+    if (participantes.length === 0) {
+        return;
+    }
+    // participantes.sort();
 
 
     while (part.firstChild) {
@@ -77,19 +107,27 @@ function listar_participantes() {
 
     for (let i of participantes) {
         let name = document.createElement('li');
-        name.textContent = i;
+        name.textContent = `${i.nombre} (${i.id})`;
         part.appendChild(name);
-        console.log(i);
+        // console.log(i);
     }
-    console.log("En la reunión están:");
-    for (let i of participantes) {
-        console.log(i);
-    }
+    /*     console.log("En la reunión están:");
+        for (let i of participantes) {
+            console.log(i);
+        } */
 }
 
 // TO-DO
-function eliminar_participantes() {
-    
+function eliminar_participante() {
+    let eliminar = prompt("Ingresa el ID de quien quieres eliminar")
+    for (let i = 0; participantes.length > i; i += 1) {
+        console.log(i);
+        if (participantes[i].id == eliminar) {
+            console.log(`Adios ${participantes[i].nombre}`);
+            participantes.splice(i, 1);
+            sessionStorage.setItem('participantes', JSON.stringify(participantes));
+        }
+    }
 }
 
 // Cámara
