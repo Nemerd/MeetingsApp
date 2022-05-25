ba2409843383c553667fbf5bb04542edd2537455
Nemerd <xtremestripedtiger@gmail.com>
2022-05-25 01:23:50 -0300

    Ahora se muestran las imagenes de los participantes y no se duplican los IDs

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,9 @@ const modalBorraParticipantes = new bootstrap.Modal(document.getElementById('mod
 let participantes = [];
 const part = document.getElementById("participantes");
 
+const contenido = document.getElementById("contenido");
+// const contenedorVideo = document.getElementById("contenedorVideo");
+
 const camara = {
     status: false,
     camOn: document.getElementById('camOn'),
@@ -49,6 +52,7 @@ class Persona {
     }
 }
 
+
 const addPart = document.getElementById("addPart");
 const delPart = document.getElementById("delPart");
 const nuevoParticipante = document.getElementById("nuevoParticipante");
@@ -62,20 +66,18 @@ const borrarNombre = document.getElementById("borrarNombre");
 const hora = document.getElementById("hora");
 
 // Event listeners
-addPart.addEventListener("click", () => modalAgregaParticipantes.show());
+addPart.addEventListener("click", () => participantes.length >= 10 ? Swal.fire({ icon: 'error', title: 'No se admiten más participantes' }) : modalAgregaParticipantes.show());
 delPart.addEventListener("click", () => modalBorraParticipantes.show());
 camara.cam.addEventListener("click", toggle_camara);
 microfono.mic.addEventListener("click", toggle_microfono);
 agregarNombre.addEventListener("click", agregar_participante);
 nuevoParticipante.addEventListener("input", (e) => {
-    console.log(e)
     if (e.inputType === "insertLineBreak" || e.data === null) {
         agregar_participante();
     }
 });
 borrarNombre.addEventListener("click", eliminar_participante);
 borrarParticipante.addEventListener("input", (e) => {
-    console.log(e)
     if (e.inputType === "insertLineBreak" || e.data === null) {
         eliminar_participante();
     }
@@ -86,20 +88,29 @@ function actualizarHora() {
     hora.textContent = `${cal.getHours()}:${cal.getMinutes()}`;
 }
 
-function randomID() {
-    let random = Math.floor(Math.random() * 10);
-
-    if (participantes.length === 0) {
-        return random;
-    }
-
+function isIDRepetido(nuevoID) {
     for (let i of participantes) {
-        if (random === i.id) {
-            console.log(i.id);
+        console.log(`Generado: ${nuevoID}`)
+        console.log(`Chequeando ${i.id}`)
+        if (i.id === nuevoID) {
+            return true;
         } else {
-            return random;
+            continue;
         }
     }
+}
+
+function randomID() {
+    let random = 0;
+    if (participantes.length === 0) {
+        return 0;
+    }
+
+    while (isIDRepetido(random)) {
+        random = Math.floor(Math.random() * 10);
+        console.log(random);
+    }
+    return random;
 }
 
 // Participantes
@@ -133,6 +144,20 @@ function listar_participantes() {
 }
 
 function mostrar_imagenes_participantes() {
+    contenido.innerHTML = ""
+    participantes.forEach(
+        (x) => contenido.innerHTML += `<div id="contenedorVideo" class="contenedorVideo">
+        <svg xmlns="http://www.w3.org/2000/svg"
+            class="videoDeParticipantes icon icon-tabler icon-tabler-user escondido" width="44" height="44"
+            viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round"
+            stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <circle cx="12" cy="7" r="4" />
+            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+        </svg>
+        <img src="./placeholders/${x.imagen}.jpg" alt="Imagen del participante" class="imagen">
+        </div>`
+    )
 
 };
 
@@ -153,7 +178,9 @@ function eliminar_participante() {
 // Cámara
 function toggle_camara() {
     camara.status = !camara.status;
-    console.log(`Cámara prendida: ${camara.status}`);
+    // console.log(`Cámara prendida: ${camara.status}`);
+
+
     camara.camOn.classList.toggle('visible', camara.status);
     camara.camOff.classList.toggle('visible', !camara.status);
 }
@@ -167,6 +194,9 @@ function toggle_microfono() {
 }
 
 actualizarHora()
+// modalAgregaParticipantes.show()
 listar_participantes()
+mostrar_imagenes_participantes()
+window.setInterval(mostrar_imagenes_participantes, 2000)
 window.setInterval(actualizarHora, 1000)
 window.setInterval(listar_participantes, 1000)
