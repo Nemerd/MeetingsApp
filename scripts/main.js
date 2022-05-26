980f95b05fa5b8d12396f3fc4fb2e9638910d167
Nemerd <xtremestripedtiger@gmail.com>
2022-05-26 11:18:04 -0300

    Mejorada la forma de mostrar el video de los participantes

--- scripts/main.js
+++ scripts/main.js
@@ -146,17 +146,28 @@ function listar_participantes() {
 function mostrar_imagenes_participantes() {
     contenido.innerHTML = ""
     participantes.forEach(
-        (x) => contenido.innerHTML += `<div id="contenedorVideo" class="contenedorVideo">
-        <svg xmlns="http://www.w3.org/2000/svg"
+        (x) => {
+            let contenedorVideo = document.createElement("div");
+            contenedorVideo.classList.add('contenedorVideo')
+
+            const iconoAusente = `<svg xmlns="http://www.w3.org/2000/svg"
             class="videoDeParticipantes icon icon-tabler icon-tabler-user escondido" width="44" height="44"
             viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round"
             stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <circle cx="12" cy="7" r="4" />
             <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-        </svg>
-        <img src="./placeholders/${x.imagen}.jpg" alt="Imagen del participante" class="imagen">
-        </div>`
+            </svg>`;
+
+            const imagen = document.createElement("img");
+            imagen.src = `./placeholders/${x.imagen}.jpg`;
+            imagen.alt = `Imagen del participante`;
+            imagen.classList.add('imagen');
+
+            contenedorVideo.innerHTML += iconoAusente;
+            contenedorVideo.appendChild(imagen);
+            contenido.appendChild(contenedorVideo);
+        }
     )
 
 };
@@ -197,6 +208,6 @@ actualizarHora()
 // modalAgregaParticipantes.show()
 listar_participantes()
 mostrar_imagenes_participantes()
-window.setInterval(mostrar_imagenes_participantes, 2000)
+window.setInterval(mostrar_imagenes_participantes, 20000)
 window.setInterval(actualizarHora, 1000)
 window.setInterval(listar_participantes, 1000)
